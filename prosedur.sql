32eeeab39600f482b8e2ada6fb7e425fec1a8d75
Mita_rz <gideonmcp080506@gmail.com>
2026-03-23 22:51:38 +0700

    Create payment validation trigger for transactions
    Replaced stock deduction trigger with payment validation trigger. Added error handling for insufficient payment.

--- prosedur.sql
+++ prosedur.sql
@@ -1,25 +1,25 @@
 DELIMITER //
-CREATE TRIGGER  pengurangan_stok_otomatis
-BEFORE INSERT ON detail_transaksi
-FOR EACH ROW 
-BEGIN 
-    DECLARE stok_sekarang INT;
-    DECLARE pesan_error VARCHAR(50);
-    SELECT stok INTO stok_sekarang FROM BARANG  
-    WHERE id_barang=NEW.id_barang;
+-- dibawah ini trigger nya
+CREATE TRIGGER validasi_pembayaran
+BEFORE INSERT ON pembayaran
+FOR EACH ROW
+BEGIN
+    DECLARE total INT;
+    DECLARE error_message VARCHAR(50);
+    SELECT total_pembayaran INTO total
+    FROM transaksi
+    WHERE id_transaksi = NEW.id_transaksi;
 
-    IF NEW.jumlah > stok_sekarang THEN
-        SET pesan_error = CONCAT ("PROSES GAGAL !!, STOK YANG ADA TERSISA ",stok_sekarang);
-         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = pesan_error;
-   
-    ELSE 
-        UPDATE barang 
-        SET stok = stok - NEW.jumlah
-        WHERE id_barang = NEW.id_barang;
-    END IF; 
-END //
+    IF NEW.jumlah_bayar < total THEN
+        SET error_message = 'Pembayaran kurang dari total Belanja';
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT =error_message;
+    END IF;
+END ;  //
+
 DELIMITER ;
 
---  dibawah ini pencobaan gagal
-INSERT INTO detail_transaksi(id_transaksi,id_barang,jumlah) VALUES
-(1,4,1000000);
+-- dibawah ini percobaan gagal 
+INSERT INTO transaksi(id_pelanggan, id_kasir,total_pembayaran) 
+VALUES  (1,2,50000);
+INSERT INTO pembayaran (id_transaksi,jumlah_bayar,  metode_pembayaran) 
+VALUES (8,911,'TUNAI');
